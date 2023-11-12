856530b1b7f30e6f4b3f1f13072069e0130403b7
BrunoBassoo <brunobasso.contato@gmail.com>
2023-11-12 16:03:14 -0300

    fiz a heranca para o gerente

--- src/model/Gerente.java
+++ src/model/Gerente.java
@@ -5,5 +5,18 @@ package model;
  * @author uniebrunosilva
  */
 public class Gerente extends Pessoa{
-    private String nome,cpf,senha;
+    public Gerente() {
+    }
+
+    public Gerente(String nome, String cpf, String senha) {
+        super(nome, cpf, senha);
+    }
+
+    public Gerente(String cpf, String senha) {
+        super(cpf, senha);
+    }
+
+    public Gerente(String cpf) {
+        super(cpf);
+    }
 }
